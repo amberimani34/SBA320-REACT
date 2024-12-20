8093a4c545510fa0af7ea9c4be13bedf652b6696
Amber Roberts <a.roberts_90@yahoo.com>
2024-12-19 19:33:28 -0600

    got api for WinePairings to console.log correctly

--- src/pages/WinePairings.jsx
+++ src/pages/WinePairings.jsx
@@ -1,6 +1,29 @@
-import React from 'react';
+import { useState, useEffect } from 'react'
 
 export default function WinePairings () {
+    const [winePairings, setWinePairings] = useState({});
+    // this is an object with a lot of keys to describe an individual pokemon
+    const [winePairing, setWinePairing] = useState({});
+
+    // const apiKey = f41fc9df027e4e078cee29c53f08f837
+  
+    let url = `https://api.spoonacular.com/food/wine/dishes?apiKey=f41fc9df027e4e078cee29c53f08f837&wine=malbec`;
+  
+    // step 1 - get the list of Pokemon from the api
+    const getWinePairings = async (url) => {
+      console.log(url);
+      try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        setWinePairings(data);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+
+    useEffect(() => {getWinePairings(url)}, [])
+
     return (
         <>
         <div>Wine Pairings</div>
